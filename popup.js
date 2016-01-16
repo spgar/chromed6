9ae4ceb9de91bdc0713edb66c32b6b35f5d8dcb2
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-01-16 16:28:14 -0500

    Fixes tabs.

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 // Shows a d6 random number.
 
 function getRandInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('result').textContent = getRandInt(1, 6);
+    document.getElementById('result').textContent = getRandInt(1, 6);
 });
